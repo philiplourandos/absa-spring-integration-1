f71741143f10562d8ddacb285978d264f9c13e84
Bevynne Maharaj <bevynne@Bevynnes-MacBook-Pro.local>
2017-01-11 16:08:52 +0200

    We made it work!!

---
+++ src/main/java/com/absa/training/olsen/service/TestService.java
@@ -0,0 +1,18 @@
+package com.absa.training.olsen.service;
+
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by bevynne on 2017/01/11.
+ */
+@Service
+public class TestService {
+    public void alpha(Message msg) {
+        System.out.println("Alhpa file: " + msg.getPayload().toString());
+    }
+
+    public void number(Message  msg) {
+        System.out.println("Numeric file: " + msg.getPayload().toString());
+    }
+}
